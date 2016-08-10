586b4a9336e50fdba951123ef06cd5ce17840eee
Markos Rendell <markosrendell@gmail.com>
2016-08-10 15:30:46 +0100

    repo name fix
    Signed-off-by: Markos Rendell <markosrendell@gmail.com>

--- jenkins/jobs/dsl/alxeia_pipeline_jobs.groovy
+++ jenkins/jobs/dsl/alxeia_pipeline_jobs.groovy
@@ -3,7 +3,7 @@ def workspaceFolderName = "${WORKSPACE_NAME}"
 def projectFolderName = "${PROJECT_NAME}"
 
 // Variables
-def referenceAppGitRepo = "alexia-example"
+def referenceAppGitRepo = "alexia"
 def referenceAppGitUrl = "ssh://jenkins@gerrit:29418/${PROJECT_NAME}/" + referenceAppGitRepo
 
 // Jobs
